46366b681d3e7285c1586da171f9d70362951cb8
LinmeiJ <linmeijiang@hotmail.com>
2019-05-21 23:50:05 -0400

    initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function calculator()
+{
+    var num1 = document.getElementById('n1').value;
+    var num2 = document.getElementById('n2').value; 
+    document.getElementById('result').value = parseInt(num1)*parseInt(num2);
+     
+   }
